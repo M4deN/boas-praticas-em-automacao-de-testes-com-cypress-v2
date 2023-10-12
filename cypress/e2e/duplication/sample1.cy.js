4712a77cbb9fad8ef7d64ede721066098de0180c
M4deN <alexdesaran@outlook.com>
2023-10-12 10:10:46 -0300

    Update sample1.cy.js

--- cypress/e2e/duplication/sample1.cy.js
+++ cypress/e2e/duplication/sample1.cy.js
@@ -1,38 +1,28 @@
 describe('Code duplication bad practice - repetitive steps', () => {
-  it('searches by typing and hitting enter', () => {
+  beforeEach(() => {
     cy.intercept(
       'GET',
       '**/search**'
     ).as('getStories')
-
     cy.visit('https://hackernews-seven.vercel.app')
     cy.wait('@getStories')
-
     cy.get('input[type="text"]')
-      .should('be.visible')
-      .and('have.value', 'redux')
-      .clear()
-      .type('frontend testing{enter}')
+    .should('be.visible')
+    .as('searchField')
+    .and('have.value', 'redux')
+    .clear()
 
+  })
+  it('searches by typing and hitting enter', () => {   
+
+    cy.get('@searchField').type('frontend testing{enter}')
     cy.wait('@getStories')
-
     cy.get('.table-row')
       .should('have.length', 100)
   })
 
-  it('searches by typing and pressing the search button', () => {
-    cy.intercept(
-      'GET',
-      '**/search**'
-    ).as('getStories')
-
-    cy.visit('https://hackernews-seven.vercel.app')
-    cy.wait('@getStories')
-
-    cy.get('input[type="text"]')
-      .should('be.visible')
-      .and('have.value', 'redux')
-      .clear()
+  it('searches by typing and pressing the search button', () => {    
+    cy.get('@searchField')
       .type('frontend testing')
 
     cy.contains('button', 'Search')
